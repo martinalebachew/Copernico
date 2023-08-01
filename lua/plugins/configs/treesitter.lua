b722a96fa01ad661208cf723e718a039eb206476
Martin Alebachew <martinalebachew@gmail.com>
2023-08-01 18:22:52 +0300

    Enhanced syntax highlighting
    - Added syntax highlighting to many of my frequently used formats and languages.
    - Turned on automatic installation of parsers when entering buffers with missing parsers.

--- lua/plugins/configs/treesitter.lua
+++ lua/plugins/configs/treesitter.lua
@@ -1,5 +1,6 @@
 local options = {
-  ensure_installed = { "lua" },
+  ensure_installed = { "bash", "c", "c_sharp", "comment", "cpp", "css", "dart", "dockerfile", "gitignore", "html", "java", "javascript", "json", "latex", "lua", "make", "markdown", "markdown_inline", "ninja", "proto", "python", "regex", "rust", "sql", "swift", "tsx", "typescript", "vim", "yaml" },
+  auto_install = true,
 
   highlight = {
     enable = true,
