3bc16327cd6a9183342d234e4e592ac13f5b4506
Martin Alebachew <49053222+martinalebachew@users.noreply.github.com>
2023-08-01 17:39:12 +0300

    Removed example configuration prompt
    - Removed example configuration prompt.

--- lua/core/bootstrap.lua
+++ lua/core/bootstrap.lua
@@ -40,23 +40,12 @@ M.gen_chadrc_template = function()
     local path = vim.fn.stdpath "config" .. "/lua/custom/"
     local input = "N"
 
-    if next(vim.api.nvim_list_uis()) then
-      input = vim.fn.input "Do you want to install example custom config? (y/N) : "
-    end
+    -- use very minimal chadrc
+    vim.fn.mkdir(path, "p")
 
-    -- clone example_config repo
-    if input == "y" then
-      M.echo "cloning example custom config repo ..."
-      shell_call { "git", "clone", "--depth", "1", "https://github.com/NvChad/example_config", path }
-      vim.fn.delete(path .. ".git", "rf")
-    else
-      -- use very minimal chadrc
-      vim.fn.mkdir(path, "p")
-
-      local file = io.open(path .. "chadrc.lua", "w")
-      file:write "---@type ChadrcConfig \n local M = {}\n M.ui = {theme = 'onedark'}\n return M"
-      file:close()
-    end
+    local file = io.open(path .. "chadrc.lua", "w")
+    file:write "---@type ChadrcConfig \n local M = {}\n M.ui = {theme = 'onedark'}\n return M"
+    file:close()
   end
 end
 
