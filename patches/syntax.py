a87535f00bd5801b731fc96924e2ac05145d1578
Martin Alebachew <martinalebachew@gmail.com>
2023-09-04 22:39:42 +0300

    Implemented patch: Enhanced Syntax Highlighting
    - Includes pre-installed parsers for many languages and automatic installation of missing parsers.

---
+++ patches/syntax.py
@@ -0,0 +1,24 @@
+from os.path import join
+from utils.patching import replace_lines
+
+NAME = "Enhanced Syntax Highlighting"
+
+
+old_options_block = r"""
+  ensure_installed = { "lua" },
+"""
+
+new_options_block = r"""
+  ensure_installed = {
+    "bash", "c", "c_sharp", "comment", "cpp", "css", "dart", "dockerfile",
+    "gitignore", "html", "java", "javascript", "json", "latex", "lua", "make",
+    "markdown", "markdown_inline", "ninja", "proto", "python", "regex", "rust",
+    "sql", "swift", "tsx", "typescript", "vim", "yaml"
+  },
+  auto_install = true,
+"""
+
+
+def patch(nvim_dir):
+  treesitter_file = join(nvim_dir, "lua/plugins/configs/treesitter.lua")
+  replace_lines(treesitter_file, old_options_block, new_options_block)
