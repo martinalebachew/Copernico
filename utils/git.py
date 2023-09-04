6c9ce741faca3c88e3955bc522f26d01b0ff9901
Martin Alebachew <martinalebachew@gmail.com>
2023-09-04 14:32:56 +0300

    Updated git.clone

--- utils/git.py
+++ utils/git.py
@@ -1,9 +1,17 @@
 from git import Repo
+from utils.logging import *
 
-def clone(github_specifier, to_path, branch="main", shallow=False):
+def clone(github_specifier, path, branch="main", shallow=False):
   url = f"https://github.com/{github_specifier}"
 
-  if shallow:
-    Repo.clone_from(url, to_path, branch=branch, depth=1)
-  else:
-    Repo.clone_from(url, to_path, branch=branch)
+  try:
+    if shallow:
+      Repo.clone_from(url, path, branch=branch, depth=1)
+    else:
+      Repo.clone_from(url, path, branch=branch)
+
+  except:
+    print_error(f"Failed to clone {url} -> {path}")
+    exit()
+
+  print_success(f"Successfully cloned {url} -> {path}")
