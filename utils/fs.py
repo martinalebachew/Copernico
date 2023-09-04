e13ed917e286cf900e8d6e8842e2b7f1f00c489c
Martin Alebachew <martinalebachew@gmail.com>
2023-09-04 14:00:55 +0300

    Implemented move

--- utils/fs.py
+++ utils/fs.py
@@ -11,3 +11,16 @@ def remove_directory(path, ignore_file_not_found=True):
       exit()
     
   print_success(f"Successfully removed {path}")
+
+
+def move(source, destination, ignore_file_not_found=True):
+  try:
+    shutil.move(source, destination)
+
+  except Exception as error:
+    if not (isinstance(error, FileNotFoundError) and ignore_file_not_found):
+      print_error(f"Failed to move {source} -> {destination}")
+      exit()
+    
+  print_success(f"Successfully moved {source} -> {destination}")
+  
