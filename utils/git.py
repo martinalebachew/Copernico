33c51ab2d8a49c818500eb6d8a348072705ae038
Martin Alebachew <martinalebachew@gmail.com>
2023-09-04 17:21:19 +0300

    Changed git.clone implementation

--- utils/git.py
+++ utils/git.py
@@ -1,4 +1,4 @@
-from git import Repo
+from subprocess import Popen, PIPE, STDOUT
 from utils.logging import *
 from utils.fs import resolve_path
 
@@ -6,14 +6,12 @@ def clone(github_specifier, path, branch="main", shallow=False):
   path = resolve_path(path)
   url = f"https://github.com/{github_specifier}"
 
-  try:
-    if shallow:
-      Repo.clone_from(url, path, branch=branch, depth=1)
-    else:
-      Repo.clone_from(url, path, branch=branch)
-
-  except:
+  git_command = f"git clone --progress {url} {path}"
+  git_command += " --depth 1" if shallow else ""
+  git = Popen(git_command, shell=True, stdout=PIPE, stderr=STDOUT, text=True)
+  
+  if git.stdout.read().endswith("done.\n"):
+    print_success(f"Successfully cloned {url} -> {path}")
+  else:
     print_error(f"Failed to clone {url} -> {path}")
     exit()
-
-  print_success(f"Successfully cloned {url} -> {path}")
