dc87cffcd682dd5cfcd9be8bf3b38b5c843286b5
Martin Alebachew <martinalebachew@gmail.com>
2023-09-04 21:36:13 +0300

    Implemented patch: No Example Configuration Prompt

---
+++ patches/no_config.py
@@ -0,0 +1,42 @@
+from os.path import join
+from utils.patching import replace_lines
+
+NAME = "No Example Configuration Prompt"
+
+old_block = r"""
+  if fn.isdirectory(path) ~= 1 then
+    local input = fn.input "Do you want to install example custom config? (y/N): "
+
+    if input:lower() == "y" then
+      M.echo "Cloning example custom config repo..."
+      shell_call { "git", "clone", "--depth", "1", "https://github.com/NvChad/example_config", path }
+      fn.delete(path .. "/.git", "rf")
+    else
+      -- use very minimal chadrc
+      fn.mkdir(path, "p")
+
+      local file = io.open(path .. "/chadrc.lua", "w")
+      if file then
+        file:write "---@type ChadrcConfig\nlocal M = {}\n\nM.ui = { theme = 'onedark' }\n\nreturn M"
+        file:close()
+      end
+    end
+  end
+"""
+
+new_block = r"""
+  if fn.isdirectory(path) ~= 1 then
+    -- use very minimal chadrc
+    fn.mkdir(path, "p")
+
+    local file = io.open(path .. "/chadrc.lua", "w")
+    if file then
+      file:write "---@type ChadrcConfig\nlocal M = {}\n\nM.ui = { theme = 'onedark' }\n\nreturn M"
+      file:close()
+    end
+  end
+"""
+
+def patch(nvim_dir):
+  bootstrap_file = join(nvim_dir, "lua/core/bootstrap.lua")
+  replace_lines(bootstrap_file, old_block, new_block)
