2a4f5ae55a7edb8e7d1055d0992ed5663d68ce20
Martin Alebachew <martinalebachew@gmail.com>
2023-09-05 07:40:06 +0300

    Updated Patch class file references

--- patches/__init__.py
+++ patches/__init__.py
@@ -7,7 +7,7 @@ patches = []
 patch_files = glob(join(dirname(__file__), "*.py"))
 
 for file in patch_files:
-  if isfile(file) and basename(file) not in ["__init__.py", "format.py"]:
+  if isfile(file) and basename(file) not in ["__init__.py", "patch.py"]:
     module_name = "patches." + basename(file)[:-3]
     module_obj = import_module(module_name)
     patches.append(Patch(basename(file), module_obj.NAME, module_obj.patch))
