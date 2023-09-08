81cf5b4916bba8854cdfc07a06228b01b75cce7c
Martin Alebachew <martinalebachew@gmail.com>
2023-09-08 17:34:51 +0300

    Added output and return-code checking to run_script()

--- utils/shell.py
+++ utils/shell.py
@@ -1,4 +1,5 @@
 from subprocess import Popen, PIPE, STDOUT
+from utils.logging import *
 
 def run_shell(command):
   process = Popen(command, shell=True, stdout=PIPE, stderr=STDOUT, text=True)
@@ -8,4 +9,11 @@ def run_shell(command):
 
 
 def run_script(path):
-  return run_shell(path)
+  return_code, output = run_shell(path)
+  if return_code == 0:
+    print_success(f"Ran script {path}")
+  else:
+    print_error(f"Failed to run script {path}")
+    exit()
+
+  return output
