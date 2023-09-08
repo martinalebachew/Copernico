836ac796b748318b1f5d329e2590bed3531d3ff9
Martin Alebachew <martinalebachew@gmail.com>
2023-09-08 17:30:45 +0300

    Implemented utils.shell.run_script()

--- utils/shell.py
+++ utils/shell.py
@@ -2,8 +2,10 @@ from subprocess import Popen, PIPE, STDOUT
 
 def run_shell(command):
   process = Popen(command, shell=True, stdout=PIPE, stderr=STDOUT, text=True)
-  process.wait()
+  return_code = process.wait()
   output = process.stdout.read()
-  return_code = process.returncode
-  
   return (return_code, output)
+
+
+def run_script(path):
+  return run_shell(path)
