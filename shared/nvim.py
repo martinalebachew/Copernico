87558a4fc3266d241091454b9bdc90d16f03d28a
Martin Alebachew <martinalebachew@gmail.com>
2023-09-14 18:45:02 +0300

    Added minimum version to Neovim constants

--- shared/nvim.py
+++ shared/nvim.py
@@ -4,3 +4,5 @@ default_dir = resolve_path("~/.config/nvim")
 backup_dir = resolve_path("~/.config/nvim.backup")
 data_dir = resolve_path("~/.local/share/nvim")
 nvchad_repo = "NvChad/NvChad"
+
+mininum_version = "0.9.0"
