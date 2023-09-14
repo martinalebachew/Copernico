3b285e90b36ea42ecec870e02fae69d7eed7368f
Martin Alebachew <martinalebachew@gmail.com>
2023-09-14 18:46:50 +0300

    Implemented check_nvim_version()

--- utils/nvim.py
+++ utils/nvim.py
@@ -1,8 +1,17 @@
 from utils.shell import *
+from utils.logging import *
 from utils.version import *
+from shared.nvim import mininum_version
 
 def get_nvim_version():
   _, output = run_shell("nvim --version")
   version_string = output.splitlines()[0].split("NVIM v")[0]
   version = parse_version(version_string)
   return version
+
+
+def check_nvim_version():
+  current_version = get_nvim_version()
+  if not is_version_sufficient(current_version, mininum_version):
+    print_error(f"Minimum required version of Neovim is {mininum_version}")
+    exit()
