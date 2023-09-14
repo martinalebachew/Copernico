b2e105e724f82e695b7edf2d51fd891da9f9cc93
Martin Alebachew <martinalebachew@gmail.com>
2023-09-14 18:41:19 +0300

    Implemented parse_version()

--- utils/version.py
+++ utils/version.py
@@ -1,5 +1,19 @@
+from utils.logging import *
+
 class Version:
   def __init__(self, major, minor, patch):
     self.major = major
     self.minor = minor
     self.patch = patch
+
+
+def parse_version(version_string):
+  try:
+    version = version_string.split(".")
+    major = int(version_string[0])
+    minor = int(version_string[1])
+    patch = int(version_string[2])
+
+  except:
+    print_error("Failed to parse version")
+    exit()
